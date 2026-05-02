302ae7fac2676e9ac63fd783ef6f7aadda392991
dhavalparmar1610 <parmardhaval375@gmail.com>
2026-05-02 10:45:02 +0530

    feat: create CarPlayUI component for managing and playing YouTube playlists

--- components/CarPlayUI.jsx
+++ components/CarPlayUI.jsx
@@ -346,6 +346,7 @@ export default function CarPlayUI({ youtubeToken, existingPlaylists, isMini }) {
           )}
         </div>
       </div>
+    )}
 
       {addingVideoId && (
         <div style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', background: 'rgba(0,0,0,0.8)', zIndex: 100, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
